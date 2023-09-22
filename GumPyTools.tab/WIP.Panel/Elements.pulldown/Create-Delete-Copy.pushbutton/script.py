beee65ba0631f77eace022d718139a9a3c2f023c
jmark <86187998+jmsgxx@users.noreply.github.com>
2023-09-22 09:08:59 +0800

    morning change
    create text note added. fixed transaction statement

--- GumPyTools.tab/WIP.Panel/Elements.pulldown/Create-Delete-Copy.pushbutton/script.py
+++ GumPyTools.tab/WIP.Panel/Elements.pulldown/Create-Delete-Copy.pushbutton/script.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__title__ = 'Filtered Element Collector'
+__title__ = 'Create/Delete/Copy Elements'
 __doc__ = """
-This script will collect elements.
+This script will create, delete and copy elements.
 __________________________________
 Author: Joven Mark Gumana
+20230922
 """
 
 
@@ -29,7 +30,7 @@ uidoc    = __revit__.ActiveUIDocument
 app      = __revit__.Application
 
 active_view     = doc.ActiveView
-active_level    = doc.ActiveView.Level
+# active_level    = doc.ActiveView.Level
 
 
 
@@ -40,16 +41,31 @@ active_level    = doc.ActiveView.Level
 with Transaction(doc, __title__) as t:
     t.Start()
 # CHANGE HERE
-
-
+# =========================================================================================================
 # ╔╦╗╔═╗═╗ ╦╔╦╗
 #  ║ ║╣ ╔╩╦╝ ║ 
 #  ╩ ╚═╝╩ ╚═ ╩ #text
+# =========================================================================================================
+    """
+    Text Note CLass
+    public static TextNote Create(
+        Document document,
+        ElementId viewId,
+        XYZ position,
+        string text,
+        ElementId typeId
+    )
+    """
+    text_type_id = FilteredElementCollector(doc).OfClass(TextNoteType).FirstElementId()
+    pt = XYZ(0,0,0)
+    text = 'This is your first create method in Revit API.'
 
-
+    TextNote.Create(doc, active_view.Id, pt, text, text_type_id)
+# ============================================================================================================
 # ╦═╗╔═╗╔═╗╔╦╗
 # ╠╦╝║ ║║ ║║║║
 # ╩╚═╚═╝╚═╝╩ ╩#room
+
 
 
 
@@ -105,4 +121,4 @@ with Transaction(doc, __title__) as t:
 # ═╩╝╚═╝╩═╝╚═╝ ╩ ╚═╝  ╚═╝╩═╝╚═╝╩ ╩╚═╝╝╚╝ ╩ ╚═╝#delete elements
 
 
-t.Commit()
+    t.Commit()
