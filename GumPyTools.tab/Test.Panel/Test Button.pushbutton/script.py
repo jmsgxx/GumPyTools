da1d1c225b85786a5e4b22327835ca5a855c7f40
jmark <86187998+jmsgxx@users.noreply.github.com>
2023-10-30 13:23:49 +0800

    wall fin to room added
    30 Oct 2023

--- GumPyTools.tab/Test.Panel/Test Button.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test Button.pushbutton/script.py
@@ -13,7 +13,7 @@ Author: Joven Mark Gumana
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ # imports
 # ===================================================================================================
 from Autodesk.Revit.DB import *
-from pyrevit import forms
+from pyrevit import forms, revit
 from System.Collections.Generic import List
 
 import os
@@ -44,37 +44,74 @@ active_view     = doc.ActiveView
 active_level    = doc.ActiveView.GenLevel
 
 
-list_of_categories = List[BuiltInCategory](
-            [BuiltInCategory.OST_SpecialityEquipment,
-             BuiltInCategory.OST_Furniture,
-             BuiltInCategory.OST_MedicalEquipment]
-)
+with forms.WarningBar(title='Pick an element:'):
+    selected_room = revit.pick_element()
 
-multi_cat_filter = ElementMulticategoryFilter(list_of_categories)
+el_cat          = selected_room.Category.Name
 
-element_list = FilteredElementCollector(doc).WhereElementIsElementType().WherePasses(multi_cat_filter).ToElementIds()
+if el_cat != 'Rooms':
+    forms.alert('Just pick a Room', exitscript=True)
 
-collect_views = FilteredElementCollector(doc).OfClass(ViewSheet).ToElements()
+calculator      = SpatialElementGeometryCalculator(doc)
+results         = calculator.CalculateSpatialElementGeometry(selected_room)
+space_solid     = results.GetGeometry()
 
-for view in collect_views:
-    placed_views = view.GetAllPlacedViews()
-    print(placed_views)  # Add this line to check if placed_views is populated correctly
-    plan_views = []
-    for placed_view in placed_views:
-        view_element = doc.GetElement(placed_view)
-        view_object = view_element.View
-        if view_object.ViewType == ViewType.ViewPlan:
-            plan_views.append(placed_view)
-    print(plan_views)
+# ╔╦╗╔═╗╦╔╗╔
+# ║║║╠═╣║║║║
+# ╩ ╩╩ ╩╩╝╚╝#main
+# =========================================================================================================
+with Transaction(doc, __title__) as t:
+    t.Start()
 
+    wall_list = []  # list of wall that has "FIN" on wall.Name
+    mark_wall = []
+    type_mark_wall = []
+    desc_wall = []
 
+    for face in space_solid.Faces:
+        spatial_sub_face_list = results.GetBoundaryFaceInfo(face)
+        if len(spatial_sub_face_list) == 0:
+            continue
 
+        for sub_face in spatial_sub_face_list:
+            host_id     = sub_face.SpatialBoundaryElement.HostElementId
+            wall        = doc.GetElement(host_id)
+            if "FIN" in wall.Name:
+                wall_list.append(wall)
 
+    # room parameter
+    room_data_set_param = selected_room.LookupParameter('Room Wall Data Set 1')
 
-# with Transaction(doc, __title__) as t:
-#     t.Start()
-#
-#
-#     t.Commit()
+    for wall in wall_list:
+        wall_mark               = wall.get_Parameter(BuiltInParameter.DOOR_NUMBER)
+        # type element
+        wall_type_id            = wall.GetTypeId()
+        wall_type               = doc.GetElement(wall_type_id)
+        wall_type_mark          = wall_type.get_Parameter(BuiltInParameter.WINDOW_TYPE_ID)
+        wall_type_description   = wall_type.get_Parameter(BuiltInParameter.ALL_MODEL_DESCRIPTION)
+
+        # append to list as string
+        mark_wall.append(wall_mark.AsValueString())
+        type_mark_wall.append(wall_type_mark.AsValueString())
+        desc_wall.append(wall_type_description.AsValueString())
+
+    room_wall_data          = list(zip(mark_wall, type_mark_wall, desc_wall))
+    room_wall_data_sorted   = sorted(room_wall_data)
+
+    # initialize an empty string
+    room_data_string = ""
+    for item in room_wall_data_sorted:
+        room_data_string += "{}\t{}\t{}\n".format(item[0], item[1], item[2])
+
+    room_data_set_param.Set(room_data_string)
+
+    t.Commit()
+# =============================================================================================
+room_name = selected_room.LookupParameter('Name')
+
+print("ROOM NAME: {}".format(room_name.AsValueString().upper()))
+print('=' * 50)
+print("WALL DATA TRANSFERRED: \n {}".format(room_data_set_param.AsString()))
+
 
 
