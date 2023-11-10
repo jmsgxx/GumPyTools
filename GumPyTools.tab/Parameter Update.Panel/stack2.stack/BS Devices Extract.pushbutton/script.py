cc30fd2d6342e95e772b645b65a11e6b4e5fb2b4
jmark <86187998+jmsgxx@users.noreply.github.com>
2023-11-10 18:31:44 +0800

    end of day push

--- GumPyTools.tab/Parameter Update.Panel/stack2.stack/BS Devices Extract.pushbutton/script.py
+++ GumPyTools.tab/Parameter Update.Panel/stack2.stack/BS Devices Extract.pushbutton/script.py
@@ -11,11 +11,13 @@ needed on BS elements such as:
 - Nurse Call Devices
 ========================================
 HOW TO:
-1. Opem MEP model
+1. Open MEP model
 2. Go to the desired Level
 3. Make sure AR Link is loaded and 
 visible in the view.
-4. Click the button and let it run.
+4. Click the button, select a file
+destination with a file name of your
+choice and let it run.
 A prompt will confirm that Excel file is
 created.
 
@@ -43,11 +45,10 @@ Author: Joven Mark Gumana
 # ===================================================================================================
 from Autodesk.Revit.DB import *
 from System.Collections.Generic import List
-import xlsxwriter
-import pyrevit
 from pyrevit import script, forms, revit
 from datetime import datetime
 
+import xlsxwriter
 import clr
 clr.AddReference("System")
 
@@ -65,6 +66,26 @@ active_level    = doc.ActiveView.GenLevel
 # phase
 all_phase = list(doc.Phases)
 phase = (all_phase[1])
+
+# output window
+output = script.get_output()
+output.center()
+
+
+# ╔═╗═╗ ╦╔═╗╔═╗╦
+# ║╣ ╔╩╦╝║  ║╣ ║
+# ╚═╝╩ ╚═╚═╝╚═╝╩═╝
+# ===========================================================================================================
+# ⭕ PREPARE EXCEL EXPORT
+file_path = forms.save_excel_file(title='Select destination file')
+# workbook = xlsxwriter.Workbook(r'C:\Users\gary_mak\Documents\GitHub\GumPyTools.extension\Output\Data Devices from MEP.xlsx')
+workbook = xlsxwriter.Workbook(file_path)
+worksheet = workbook.add_worksheet()
+headings = ['Element ID', 'Category', 'Family', 'Type', 'Image', 'PosX', 'PosY', 'PosZ', 'Room']
+for i, heading in enumerate(headings):
+    worksheet.write(0, i, heading)
+
+# ===========================================================================================================
 
 # 🟩 BS elements
 list_of_categories = List[BuiltInCategory]([
@@ -93,14 +114,6 @@ for link in all_links:
 linked_doc = ar_model.GetLinkDocument()
 all_rooms_in_link_level = FilteredElementCollector(linked_doc).OfCategory(BuiltInCategory.OST_Rooms).WhereElementIsNotElementType().ToElements()
 
-
-# ⭕ PREPARE EXCEL EXPORT
-workbook = xlsxwriter.Workbook(r'C:\Users\gary_mak\Documents\GitHub\GumPyTools.extension\Output\Data Devices from MEP.xlsx')
-worksheet = workbook.add_worksheet()
-headings = ['Element ID', 'Category', 'Family', 'Type', 'Image', 'PosX', 'PosY', 'PosZ', 'Room']
-for i, heading in enumerate(headings):
-    worksheet.write(0, i, heading)
-
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
 # ╩ ╩╩ ╩╩╝╚╝#main
@@ -108,49 +121,52 @@ for i, heading in enumerate(headings):
 with Transaction(doc, __title__) as t:
 
     t.Start()
-    # # 🟢 ROOM
-    # for room_link in all_rooms_in_link_level:
-    #     if room_link and room_link.Location:    # exclude the unplaced rooms
-    #         room_loc = room_link.Location.Point
-    #
-    #         room_copy = doc.Create.NewRoom(active_level, UV(room_loc.X, room_loc.Y))
-    #         #  name
-    #         room_copy_name = room_copy.get_Parameter(BuiltInParameter.ROOM_NAME)
-    #         rm_link_name = room_link.get_Parameter(BuiltInParameter.ROOM_NAME).AsValueString()
-    #         if rm_link_name:
-    #             room_copy_name.Set(str(rm_link_name.upper()))
-    #         else:
-    #             continue
-    #
-    #         # number
-    #         room_copy_number = room_copy.get_Parameter(BuiltInParameter.ROOM_NUMBER)
-    #         rm_link_number = room_link.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
-    #         if rm_link_number:
-    #             room_copy_number.Set(str(rm_link_number))
-    #         else:
-    #             continue
-    #         # print
-    #         # print("Room Name:   {}".format(rm_link_name))  # print statement to check rm_link_name
-    #         # print("Room Number: {}".format(rm_link_number))  # print statement to check rm_link_number
-    #         # print('-' * 50)
-    #
-    # # CHECK THE CURRENT MODEL AFTER THE CREATION OF TEMPORARY ROOM
-    # current_level_filter = ElementLevelFilter(active_level.Id)
-    #
-    # rooms_current = FilteredElementCollector(doc, active_view.Id).OfCategory(BuiltInCategory.OST_Rooms)\
-    #     .WherePasses(current_level_filter).WhereElementIsNotElementType().ToElements()
-    #
-    # print("Total Room in link:      {}".format(len(all_rooms_in_link_level)))
-    # print("Total Room in current:   {}".format(len(rooms_current)))
-    #
-    # for room_cur in rooms_current:
-    #     print(room_cur.get_Parameter(BuiltInParameter.ROOM_NAME).AsValueString())
-    #     print(room_cur.Number)
-    #     print('=' * 50)
+    # 🟢 ROOM
+    for room_link in all_rooms_in_link_level:
+        if room_link and room_link.Location:    # exclude the unplaced rooms
+            room_loc = room_link.Location.Point
+
+            room_copy = doc.Create.NewRoom(active_level, UV(room_loc.X, room_loc.Y))
+            #  name
+            room_copy_name = room_copy.get_Parameter(BuiltInParameter.ROOM_NAME)
+            rm_link_name = room_link.get_Parameter(BuiltInParameter.ROOM_NAME).AsValueString()
+            if rm_link_name:
+                room_copy_name.Set(str(rm_link_name.upper()))
+            else:
+                continue
+
+            # number
+            room_copy_number = room_copy.get_Parameter(BuiltInParameter.ROOM_NUMBER)
+            rm_link_number = room_link.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
+            if rm_link_number:
+                room_copy_number.Set(str(rm_link_number))
+            else:
+                continue
+            print('Creating Room.....')
+            print("Room Name:   {}".format(rm_link_name))  # print statement to check rm_link_name
+            print("Room Number: {}".format(rm_link_number))  # print statement to check rm_link_number
+            print('-' * 50)
+
+    # CHECK THE CURRENT MODEL AFTER THE CREATION OF TEMPORARY ROOM
+    current_level_filter = ElementLevelFilter(active_level.Id)
+
+    rooms_current = FilteredElementCollector(doc, active_view.Id).OfCategory(BuiltInCategory.OST_Rooms)\
+        .WherePasses(current_level_filter).WhereElementIsNotElementType().ToElements()
+    print("Total Room in link:      {}".format(len(all_rooms_in_link_level)))
+    print("Total Room in current:   {}".format(len(rooms_current)))
+
+    for room_cur in rooms_current:
+        print('ROOM CREATED')
+        print('Room Name: {}'.format(room_cur.get_Parameter(BuiltInParameter.ROOM_NAME).AsValueString()))
+        print('Room Number: {}'.format(room_cur.Number))
+        print('=' * 50)
 
 # ================================================================================================================
     # 🔵 ELEMENT
-    row = 1
+
+
+    room_numbers = []   # extracted from temp rooms
+    room_name = None
     for element in all_elements_in_level:
         if element:
             #  type param
@@ -174,40 +190,54 @@ with Transaction(doc, __title__) as t:
 
             # room of elements
             room_active = element.Room[phase]
+            room_active_name = room_active.get_Parameter(BuiltInParameter.ROOM_NAME).AsValueString()
+            room_name = room_active_name
             if room_active:
                 room_active_number  = room_active.Number
-                room_num_split      = room_active_number.split('.')
-                # print("--x---" * 3)
-                if len(room_num_split) < 4:
-                    new_room_number     = room_num_split[0]+"."+room_num_split[1]+"."+room_num_split[2]+".01"
-                    room_active_number  = new_room_number
-                #     print(room_active_number)
-                # else:
-                #     print(room_active_number)
+                room_numbers.append(room_active_number)
+
+        drawings_dict = {}  # initialize a dictionary
+        for i in range(len(room_numbers)):
+            parts = room_numbers[i].split('.')  # separate the drawing number into parts
+
+            if len(parts) == 4:  # skip those AA.BB.CC.01
+                continue
+
+            if room_numbers[i] not in drawings_dict:
+                drawings_dict[room_numbers[i]] = 1
+            else:
+                drawings_dict[room_numbers[i]] += 1
+            room_numbers[i] = "{}.{}".format(room_numbers[i], str(drawings_dict[room_numbers[i]]).zfill(2))
 
 
-            # 🆗 WRITE TO EXCEL
-            worksheet.write('A' + str(row+1), int((str(element_id))))
-            worksheet.write('B' + str(row+1), category_name)
-            worksheet.write('C' + str(row+1), family_name)
-            worksheet.write('D' + str(row+1), type_description.AsValueString())
-            worksheet.write('E' + str(row+1), type_image.AsValueString().strip())
-            worksheet.write('F' + str(row+1), pos_x)
-            worksheet.write('G' + str(row+1), pos_y)
-            worksheet.write('H' + str(row+1), pos_z)
-            worksheet.write('I' + str(row+1), room_active_number)
+    # write to Excel
+    row = 1
+    for room_number in room_numbers:
+        print('Updated Room Number:\n')
+        print(room_name.upper())
+        print(room_number)
+        print('--x--' * 4)
+        # 🆗 WRITE TO EXCEL
+        worksheet.write('A' + str(row+1), int((str(element_id))))
+        worksheet.write('B' + str(row+1), category_name)
+        worksheet.write('C' + str(row+1), family_name)
+        worksheet.write('D' + str(row+1), type_description.AsValueString())
+        worksheet.write('E' + str(row+1), type_image.AsValueString().strip())
+        worksheet.write('F' + str(row+1), pos_x)
+        worksheet.write('G' + str(row+1), pos_y)
+        worksheet.write('H' + str(row+1), pos_z)
+        worksheet.write('I' + str(row+1), room_number)
 
-            row += 1
+        row += 1
 
     workbook.close()
 
-    t.Commit()
-
-    # if t.GetStatus() == TransactionStatus.Started:
-    #     t.RollBack()
+    if t.GetStatus() == TransactionStatus.Started:
+        t.RollBack()
 # ==========================================================================================================
 
 current_datetime = datetime.now()
 time_stamp = current_datetime.strftime('%d %b %Y %H%Mhrs')
 forms.alert('Excel exported!\nTime Stamp: {}'.format(time_stamp), warn_icon=False, exitscript=False)
+
 
