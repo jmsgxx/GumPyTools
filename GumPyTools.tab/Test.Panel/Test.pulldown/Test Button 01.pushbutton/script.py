7f6506327a99380321735a5ca82bcce080a22dbd
jmark <86187998+jmsgxx@users.noreply.github.com>
2023-12-04 18:33:06 +0800

    end of day push

--- GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
@@ -13,6 +13,7 @@ Author: Joven Mark Gumana
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ # imports
 # ===================================================================================================
 from Autodesk.Revit.DB import *
+from System.Collections.Generic import List
 from pyrevit import forms, revit
 from datetime import datetime
 import pyrevit
@@ -29,4 +30,57 @@ doc      = __revit__.ActiveUIDocument.Document
 uidoc    = __revit__.ActiveUIDocument
 app      = __revit__.Application
 
+
+collector = FilteredElementCollector(doc).OfClass(ViewSheet)
+
+# 🟡 DIRECTORY TO SAVE THE FILE
+directory = r"C:\Users\gary_mak\Desktop\PDF"
+
+with Transaction(doc, __title__) as t:
+    t.Start()
+
+    my_view_set = List[ElementId]
+    for vs in collector:
+        sheet_number = vs.SheetNumber
+        sheet_name = vs.Name
+        plot_param = vs.LookupParameter('Plot Batch')
+        if plot_param and plot_param.AsString() == "RADS1":
+            my_view_set.Add(vs.Id)
+
+        view_set = List[my_view_set]
+
+
+        # Set the PDF export options
+        options = PDFExportOptions()
+        options.AlwaysUseRaster = False
+        options.ColorDepth = ColorDepthType.Color
+        options.Combine = False
+        options.ExportQuality = PDFExportQualityType.DPI4000
+        options.FileName = sheet_number + " - " + sheet_name
+        options.HideCropBoundaries = True
+        options.HideReferencePlane = True
+        options.HideScopeBoxes = True
+        options.HideUnreferencedViewTags = True
+        options.MaskCoincidentLines = True
+        options.OriginOffsetX = 0
+        options.OriginOffsetY = 0
+        options.PaperFormat = ExportPaperFormat.ISO_A3
+        options.PaperPlacement = PaperPlacementType.Center
+        options.RasterQuality = RasterQualityType.High
+        options.ReplaceHalftoneWithThinLines = False
+        options.StopOnError = True
+        options.ViewLinksInBlue = True
+        options.ZoomPercentage = 100
+        options.ZoomType = ZoomType.Zoom
+
+        try:
+            if doc.Export(directory, view_set, options):
+                # SHOW NOTIFICATION THAT PRINT IS FINISHED
+                forms.alert('Print finish.You can find the file on "Desktop/PDF" folder.', exitscript=True)
+
+        # HANDLE ERROR JUST IN CASE
+        except Exception as e:
+            print("An error occurred: {}".format(e))
+
+    t.Commit()
 
