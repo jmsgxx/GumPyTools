16b4ab64d163fadab2bdf7adf4f876211df8f7b6
jmark <86187998+jmsgxx@users.noreply.github.com>
2023-12-28 15:37:25 +0800

    added checking code

---
+++ Archive/check_rooms_if_in_sheet.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'Test Button 01'
+__doc__ = """
+This script is a test.
+__________________________________
+
+Author: Joven Mark Gumana
+"""
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗
+# ║║║║╠═╝║ ║╠╦╝ ║
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ # imports
+# ===================================================================================================
+import xlsxwriter
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Architecture import Room
+from pyrevit import forms
+import sys
+import clr
+clr.AddReference("System")
+from System.Collections.Generic import List
+
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝# variables
+# ======================================================================================================
+doc      = __revit__.ActiveUIDocument.Document  # type: Document
+uidoc    = __revit__.ActiveUIDocument
+app      = __revit__.Application
+
+active_view     = doc.ActiveView
+active_level    = doc.ActiveView.GenLevel
+current_view    = [active_view.Id]
+
+all_sheets = FilteredElementCollector(doc).OfClass(ViewSheet).ToElements()
+
+level_filter = active_level.Id
+
+rooms = FilteredElementCollector(doc)\
+            .OfCategory(BuiltInCategory.OST_Rooms)\
+            .WherePasses(ElementLevelFilter(level_filter))\
+            .ToElements()
+
+sheets_in_rad = []
+rooms_lst = []
+
+for sheet in all_sheets:    # type: ViewSheet
+    sheet_dept  = sheet.LookupParameter('Sheet Department').AsString()
+    room_dept   = sheet.LookupParameter('Room Department').AsString()
+    dwg_type    = sheet.LookupParameter('Drawing Type').AsString()
+    if sheet_dept == 'RADIOLOGY':
+        if room_dept == 'RADIOLOGY SESSION 3 LG NM & PET CT':
+            if dwg_type == 'ROOM LAYOUT SHEET':
+                sheet_number    = sheet.SheetNumber
+                sheet_name      = sheet.Name
+                sheets_in_rad.append(sheet_name)
+
+for room in rooms:
+    room_class = room.LookupParameter('Rooms_Classification_BLP').AsString()
+    room_dept_param = room.LookupParameter('Department_BLP')
+    if room_dept_param:
+        _room_dept = room_dept_param.AsString()
+        if _room_dept == 'RADIOLOGY':
+            if room_class == 'DEPARTMENTAL - BLP' or room_class == 'REPEATABLE - BLP':
+                # room_name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
+                room_name = room.LookupParameter('Room_Name_BLP').AsString()
+                room_number = room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
+                rooms_lst.append(room_name)
+
+unique_sheet = set(sheets_in_rad)
+unique_room = set(rooms_lst)
+
+missing_rm = [item for item in sorted(unique_room) if item not in unique_sheet]
+
+if len(missing_rm) == 0:
+    print('All rooms are covered.')
+else:
+    for i in missing_rm:
+        print(i)
--- GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
@@ -12,10 +12,12 @@ Author: Joven Mark Gumana
 # ║║║║╠═╝║ ║╠╦╝ ║
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ # imports
 # ===================================================================================================
-import xlsxwriter
+from Autodesk.Revit.UI.Selection import Selection, ObjectType
+from Snippets._x_selection import ISelectionFilter_Classes
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.DB.Architecture import Room
 from pyrevit import forms
+import pyrevit
 import sys
 import clr
 clr.AddReference("System")
@@ -33,46 +35,56 @@ app      = __revit__.Application
 active_view     = doc.ActiveView
 active_level    = doc.ActiveView.GenLevel
 current_view    = [active_view.Id]
+selection = uidoc.Selection     # type: Selection
 
-all_sheets = FilteredElementCollector(doc).OfClass(ViewSheet).ToElements()
+# 1️⃣ COLLECT ALL SHEETS
+sheet_collection = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).ToElements()
 
-level_filter = active_level.Id
+# -------------------------------------------------------------------------------
+# 🟢 PREPARE ROOM SELECTION
+selected_rooms = [doc.GetElement(el_id) for el_id in selection.GetElementIds()]
+if not selected_rooms:
+    filter_type = ISelectionFilter_Classes([Room])
+    selected_element = selection.PickObjects(ObjectType.Element, filter_type, "Select Room")
+    selected_rooms = [doc.GetElement(el) for el in selected_element]
+    if not selected_rooms:
+        forms.alert("Select room.", exitscript=True)
 
-rooms = FilteredElementCollector(doc)\
-            .OfCategory(BuiltInCategory.OST_Rooms)\
-            .WherePasses(ElementLevelFilter(level_filter))\
-            .ToElements()
+room_name = None
+room_number = None
 
-sheets_in_rad = []
-rooms_lst = []
+# VIEW SHEET
+room_sheets = {}
+for sheet in sheet_collection:  # type: ViewSheet
+    sheet_number = sheet.SheetNumber
+    sheet_name = sheet.Name
+    viewport_id = sheet.GetAllViewports()  # returns an id
+    for ids in viewport_id:
+        viewport = doc.GetElement(ids)  # type: Viewport
+        view_id = viewport.ViewId
 
-for sheet in all_sheets:    # type: ViewSheet
-    sheet_dept  = sheet.LookupParameter('Sheet Department').AsString()
-    room_dept   = sheet.LookupParameter('Room Department').AsString()
-    dwg_type    = sheet.LookupParameter('Drawing Type').AsString()
-    if sheet_dept == 'RADIOLOGY':
-        if room_dept == 'RADIOLOGY SESSION 3 LG NM & PET CT':
-            if dwg_type == 'ROOM LAYOUT SHEET':
-                sheet_number    = sheet.SheetNumber
-                sheet_name      = sheet.Name
-                sheets_in_rad.append(sheet_name)
-
-for room in rooms:
-    room_class = room.LookupParameter('Rooms_Classification_BLP').AsString()
-    room_dept_param = room.LookupParameter('Department_BLP')
-    if room_dept_param:
-        _room_dept = room_dept_param.AsString()
-        if _room_dept == 'RADIOLOGY':
-            if room_class == 'DEPARTMENTAL - BLP' or room_class == 'REPEATABLE - BLP':
-                # room_name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
+        view = doc.GetElement(view_id)  # type: View
+        if view.ViewType == ViewType.FloorPlan:
+            # GET THE ROOM
+            for room in selected_rooms:     # type: Room
                 room_name = room.LookupParameter('Room_Name_BLP').AsString()
-                room_number = room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
-                rooms_lst.append(room_name)
+                room_number = room.Number
+                room_key = "{} - {}".format(room_name, room_number)
+                if room_key not in room_sheets:
+                    room_sheets[room_key] = []  # Initialize list for the room
+                room_sheets[room_key].append((sheet_number, sheet_name))
 
-unique_sheet = set(sheets_in_rad)
-unique_room = set(rooms_lst)
 
-missing_rm = [item for item in sorted(unique_room) if item not in unique_sheet]
+output = pyrevit.output.get_output()
+output.center()
+output.resize(600, 750)
 
-for i in missing_rm:
-    print(i)
+for room, sheets in room_sheets.items():
+    print('-' * 50)
+    print(room)
+    print('-' * 50)
+    for item in sorted(sheets):
+        print("{} - {}".format(*item))
+    print("\n")
+    print('=' * 50)
+
