cde1d214fa81ae0ddc1ce9bf5dfade08eb4f6a01
jmark <86187998+jmsgxx@users.noreply.github.com>
2024-01-22 18:30:55 +0800

    end of day push

--- GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
@@ -35,35 +35,4 @@ active_view     = doc.ActiveView
 active_level    = doc.ActiveView.GenLevel
 
 
-# ╔╦╗╔═╗╦╔╗╔
-# ║║║╠═╣║║║║
-# ╩ ╩╩ ╩╩╝╚╝#main
-# =========================================================================================================
-def _logger(script_name):
-    # get date and time
-    time = datetime.now()
-    datestamp = str(time.strftime("%d-%m-%y"))
-    timestamp = str(time.strftime("%H:%M:%S"))
 
-    # get pc and user info
-    username = os.environ['USERNAME']
-    computer_name = os.environ['COMPUTERNAME']
-    directory = r'X:\J521\BIM\00_SKA-Tools\SKA_Tools'
-    filepath = op.join(directory, 'logger.csv')
-
-    # project info
-    project_number = doc.ProjectInformation.get_Parameter(BuiltInParameter.PROJECT_NAME).AsString()
-    project_name = doc.ProjectInformation.get_Parameter(BuiltInParameter.PROJECT_NUMBER).AsString()
-    file_name = doc.Title
-
-    # put all info parameters together in a single line and add it to a CSV file
-    try:
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        with open(filepath, 'a') as f:
-            items = [username, computer_name, script_name, project_name, project_number, file_name, datestamp, timestamp]
-            lines_to_write = '\n' + ','.join(items)
-            f.writelines(lines_to_write)
-
-    except Exception as e:
-        forms.alert(str(e))
--- GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 02.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 02.pushbutton/script.py
@@ -13,7 +13,7 @@ Author: Joven Mark Gumana
 # ===================================================================================================
 from Autodesk.Revit.DB.Architecture import Room
 from Autodesk.Revit.UI.Selection import Selection, ObjectType
-from Snippets._x_selection import ISelectionFilter_Classes, get_multiple_elements
+from Snippets._x_selection import DoorCustomFilter, get_multiple_elements
 from Snippets._context_manager import rvt_transaction, try_except
 from Autodesk.Revit.DB import *
 import pyrevit
@@ -38,38 +38,52 @@ active_level    = doc.ActiveView.GenLevel
 current_view    = [active_view.Id]
 
 # =====================================================================================================
+# door_selection = get_multiple_elements()
+#
+# if not door_selection:
+#     with try_except():
+#         filter_type = DoorCustomFilter()
+#         door_list = selection.PickObjects(ObjectType.Element, filter_type, "Select Doors")
+#         door_selection = [doc.GetElement(dr) for dr in door_list]
+#
+#         if not door_selection:
+#             forms.alert("No doors selected. Exiting command.", exitscript=True, warn_icon=False)
 
-door_selection = FilteredElementCollector(doc, active_view.Id)\
-    .OfCategory(BuiltInCategory.OST_Doors)\
-    .WhereElementIsNotElementType()\
+door_selection = FilteredElementCollector(doc, active_view.Id) \
+    .OfCategory(BuiltInCategory.OST_Doors) \
+    .WhereElementIsNotElementType() \
     .ToElements()
 
 # =================================================================================================================
-
-pull_wall   = ['PL', 'WL', 'PLS', 'WLS']
-push_track  = ['PH', 'TK', 'PHS', 'TKS']
-
+door_count = 0
 
 for door in door_selection:  # type: FamilyInstance
-    if door.Location:
-        door_type_id = door.GetTypeId()
-        door_type = doc.GetElement(door_type_id)
+    door_type_id = door.GetTypeId()
+    door_type = doc.GetElement(door_type_id)
 
-        dr_pull_wl_ht       = door_type.LookupParameter('Door Protection Pull or Wall Height Code')
-        dr_pull_wl          = door_type.LookupParameter('Door Protection Pull or Wall Code')
-        dr_push_tr_ht       = door_type.LookupParameter('Door Protection Push or Track Height Code')
-        dr_push_tr          = door_type.LookupParameter('Door Protection Push or Track Code')
+    dr_pull_wl_ht   = door_type.LookupParameter('Door Protection Pull or Wall Height Code')
+    dr_pull_wl      = door_type.LookupParameter('Door Protection Pull or Wall Code')
+    dr_push_tr_ht   = door_type.LookupParameter('Door Protection Push or Track Height Code')
+    dr_push_tr      = door_type.LookupParameter('Door Protection Push or Track Code')
 
-        door_type_name      = door_type.get_Parameter(BuiltInParameter.SYMBOL_FAMILY_NAME_PARAM).AsString()
+    dr_prot_pl_wl_yes = door_type.LookupParameter('Door Protection Pull or Wall Yes')
+    dr_prot_ph_tk_yes = door_type.LookupParameter('Door Protection Push or Track Yes')
 
-        dr_prot_pl_wl_yes      = door_type.LookupParameter('Door Protection Pull or Wall Yes')
-        dr_prot_ph_tk_yes      = door_type.LookupParameter('Door Protection Push or Track Yes')
+    door_type_name = door_type.get_Parameter(BuiltInParameter.SYMBOL_FAMILY_NAME_PARAM).AsString()
 
+    with rvt_transaction(doc, __title__):
+        # if dr_push_tr.AsString() and dr_push_tr_ht.AsString() \
+        #         and dr_pull_wl.AsString() and dr_pull_wl_ht.AsString():
+        #     dr_prot_pl_wl_yes.Set(1)
+        #     dr_prot_ph_tk_yes.Set(1)
 
-        with rvt_transaction(doc, __title__):
-            if not dr_pull_wl_ht and not dr_pull_wl and not dr_push_tr_ht and not dr_push_tr:
-                dr_prot_pl_wl_yes.Set(1)
-                dr_prot_ph_tk_yes.Set(0)
+        if not dr_push_tr.AsString() and not dr_push_tr_ht.AsString() \
+                and not dr_pull_wl.AsString() and not dr_pull_wl_ht.AsString():
+            dr_push_tr.Set('--')
 
+        # elif dr_push_tr.AsString() and dr_push_tr_ht.AsString() \
+        #         and not dr_pull_wl.AsString() and not dr_pull_wl_ht.AsString():
+        #     dr_prot_pl_wl_yes.Set(1)
+        #     dr_prot_ph_tk_yes.Set(0)
 
 
