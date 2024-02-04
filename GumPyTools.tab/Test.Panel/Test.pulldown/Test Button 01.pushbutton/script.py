1201fb789fda9748abb18f1e02c9834b6ccccd33
jmark <86187998+jmsgxx@users.noreply.github.com>
2024-02-04 20:03:05 +0800

    end of day push

--- GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 01.pushbutton/script.py
@@ -40,27 +40,9 @@ all_sheets = FilteredElementCollector(doc).OfClass(ViewSheet).ToElements()
 all_views = FilteredElementCollector(doc).OfClass(ViewPlan).ToElements()
 all_viewports = FilteredElementCollector(doc).OfClass(Viewport).ToElements()
 
-# with rvt_transaction(doc, __title__):
-#     for sheet in all_sheets:    # type: ViewSheet
-#         sheet_name = sheet.Name
-#         sheet_number = sheet.SheetNumber
-#         if sheet_number.startswith('DL0') and 'CONTENT PAGE' not in sheet_name:
-#             part_s_num = sheet_number.split("-")
-#             for view in all_views:
-#                 view_name = view.Name
-#                 if "DOC_FP_FFL_B2_50" in view_name or \
-#                         "DOC_FP_FFL_B1_50" in view_name or \
-#                         "DOC_FP_FFL_LG_50" in view_name:
-#                     view_num = view_name.split("_")[-1]
-#                     if part_s_num[1] == view_num:
-#                         print(sheet_number + "-" + sheet_name + ":" + view_name)
-#                         origin_pt = XYZ(0, 0, 0)
-#                         with try_except():
-#                             Viewport.Create(doc, sheet.Id, view.Id, origin_pt)
 
 with rvt_transaction(doc, __title__):
     with try_except():
-        x_offset = 0  # Initialize x offset for view placement
         for sheet in all_sheets:    # type: ViewSheet
             sheet_number = sheet.SheetNumber
             sheet_name = sheet.Name
@@ -71,11 +53,21 @@ with rvt_transaction(doc, __title__):
                 sheet_num = sheet_number[-2:]
                 view_num = view_name[-2:]
 
-                if "BASEMENT 2" in sheet_name and "B2" in view_name and sheet_num == view_num:
-                    print("{} : {}".format(sheet_number, view_name))
-                    print("{} : {}".format(type(sheet.Id), type(view.Id)))
-                    origin_pt = XYZ(x_offset, 0, 0)  # Use x offset for view placement
+                if "CONTENT PAGE" in sheet_name:
+                    continue
+                elif "LOWER GROUND DETAIL" in sheet_name and "LG" in view_name and sheet_num == view_num:
+                    # vp_ids = sheet.GetAllViewports()
+                    # for id in vp_ids:
+                    #     vp_el = doc.GetElement(id)
+                    #     sheet.DeleteViewport(vp_el)
+                    # print("{} : {}".format(sheet_name, view_name))
+                    sht_outline = sheet.Outline
+                    x = sht_outline.Max.U - sht_outline.Min.U
+                    y = sht_outline.Max.V - sht_outline.Min.U
+
+                    origin_pt = XYZ(x/2, y/1.8, 0)
                     Viewport.Create(doc, sheet.Id, view.Id, origin_pt)
+
 
 
 
