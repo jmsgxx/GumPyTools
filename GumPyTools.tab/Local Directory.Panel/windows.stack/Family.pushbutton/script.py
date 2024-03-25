b32669dea7a16d2ebe234a39cda954e87b90060e
jmark <86187998+jmsgxx@users.noreply.github.com>
2024-03-25 09:17:19 +0800

    addded bim fam

--- GumPyTools.tab/Local Directory.Panel/windows.stack/Family.pushbutton/script.py
+++ GumPyTools.tab/Local Directory.Panel/windows.stack/Family.pushbutton/script.py
@@ -1,7 +1,21 @@
 import os
+import clr
+clr.AddReference('System.Windows.Forms')
+from System.Windows.Forms import Clipboard
+
+
+def set_text(text):
+    Clipboard.SetText(text)
+
+
+def get_text():
+    return Clipboard.GetText()
+
 
 # Specify the path of the directory you want to open
 directory_path = r"X:\J521\BIM"
+set_text(directory_path)   # copy to clipboard
 
-# Open the directory using the default file explorer of your operating system
+
+# Open the directory
 os.startfile(directory_path)
