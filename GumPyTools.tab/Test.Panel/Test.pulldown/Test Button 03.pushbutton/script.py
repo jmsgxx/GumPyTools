9c2c8c8219f6920aaf9b8b8bea6d8e6669deb281
jmark <86187998+jmsgxx@users.noreply.github.com>
2024-05-24 21:58:50 +0800

    added grid bubble

--- GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 03.pushbutton/script.py
+++ GumPyTools.tab/Test.Panel/Test.pulldown/Test Button 03.pushbutton/script.py
@@ -11,7 +11,7 @@ Author: Joven Mark Gumana
 # ║║║║╠═╝║ ║╠╦╝ ║ 
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ # imports
 # ===================================================================================================
-import math
+from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox, Separator, Button, CheckBox)
 from Snippets._x_selection import get_multiple_elements, ISelectionFilter_Classes, CurvesFilter
 import xlrd
 from Autodesk.Revit.DB import *
@@ -41,11 +41,51 @@ selection = uidoc.Selection  # type: Selection
 
 
 # ======================================================================================================
-selected_sheets = get_multiple_elements()
+selected_grids = get_multiple_elements()
+
+if not selected_grids:
+    with try_except():
+        filter_type = ISelectionFilter_Classes([Grid])
+        grid_list = selection.PickObjects(ObjectType.Element, filter_type, "Select Wall")
+        selected_grids = [doc.GetElement(gr) for gr in grid_list]
+
+    if not selected_grids:
+        forms.alert('No wall selected', exitscript=True)
+
+bub_position = {
+    'Start Bubble': 0,
+    'End Bubble': 1
+}
+
+show_opt = {
+    'Show': 0,
+    'Hide': 1,
+}
+
+components = [Label('Bubble Position'),
+              ComboBox('bub_pos', bub_position),
+              Separator(),
+              Label('Show or Hide'),
+              ComboBox('show_hide', show_opt),
+              Separator(),
+              Button('Select')]
+form = FlexForm("What's up Jack?", components)
+form.show()
+
+user_input = form.values
+bubble = user_input['bub_pos']
+show_hide = user_input['show_hide']
 
 with rvt_transaction(doc, __title__):
-    for sheet in selected_sheets:
-        viewport_id = sheet.GetAllViewports()
-        for vp in viewport_id:
-            doc.Delete(vp)
+    for grid in selected_grids:     # type: Grid
+        if bubble == 0 and show_hide == 1:
+            grid.HideBubbleInView(DatumEnds.End0, active_view)
+        elif bubble == 0 and show_hide == 0:
+            grid.ShowBubbleInView(DatumEnds.End0, active_view)
+        elif bubble == 1 and show_hide == 1:
+            grid.HideBubbleInView(DatumEnds.End1, active_view)
+        elif bubble == 1 and show_hide == 0:
+            grid.ShowBubbleInView(DatumEnds.End1, active_view)
+
+
 
