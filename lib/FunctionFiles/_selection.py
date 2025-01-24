4936180ba5f8365a5587078a425ed6e53ff90e0a
jmark <86187998+jmsgxx@users.noreply.github.com>
2025-01-24 11:23:24 +0800

    New reusable scripts FunctionFiles

---
+++ lib/FunctionFiles/_selection.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI.Selection import Selection, ISelectionFilter, ObjectType
+from pyrevit import forms
+from Snippets._context_manager import try_except
+
+import clr
+clr.AddReference("System")
+from System.Collections.Generic import List, HashSet
+from System import Enum
+
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+app = __revit__.Application
+
+active_view = doc.ActiveView
+active_level = doc.ActiveView.GenLevel
+selection = uidoc.Selection  # type: Selection
+# =================================================================
+# ╔═╗╦  ╔═╗╔═╗╔═╗
+# ║  ║  ╠═╣╚═╗╚═╗
+# ╚═╝╩═╝╩ ╩╚═╝╚═╝
+
+
+class SelectElementBIGClass(ISelectionFilter):
+    def __init__(self, big_enum):
+        """
+        select any element provided by any built in category enum
+        elem.Category.Id == ElementId(BuiltInCategory.OST_SpecialityEquipment)
+        """
+        self.big_enum = big_enum
+
+    def AllowElement(self, elem):
+        if elem.Category.Id == ElementId(self.big_enum):
+            return True
+
+
+
+class FECollectorCat:
+    def __init__(self, big_enum, selected_view_id="", by_instance=True):
+        self.big_enum = big_enum
+        self.selected_view_id = selected_view_id
+        self.by_instance = by_instance
+
+    def get_elements(self):
+        if self.selected_view_id:
+            fec = FilteredElementCollector(doc, self.selected_view_id)
+        else:
+            fec = FilteredElementCollector(doc)
+
+        if self.by_instance:
+            elements = fec.OfCategory(self.big_enum).WhereElementIsNotElementType().ToElements()
+        else:
+            elements = fec.OfCategory(self.big_enum).WhereElementIsElementType().ToElements()
+
+        return elements
+
+# =================================================================
+# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔
+# ╠╣ ║ ║║║║║   ║ ║║ ║║║║
+# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝
+
+
+def highlight_selected_elements(element_id_lst):
+    """
+    Will highlight the selected elements
+    @param element_id_lst: element list
+    @return: selected elements
+    """
+    return selection.SetElementIds(List[ElementId](element_id_lst))
+
+
+def get_multiple_elements():
+    """get elements in selected items"""
+    return [doc.GetElement(el_id) for el_id in selection.GetElementIds()]
+
+
+def selection_filter(filter_type):
+    """
+    @param filter_type: SelectElementBIGClass
+    @return: selected element object
+    """
+    selected_els = get_multiple_elements()
+
+    if not selected_els:
+        with try_except():
+            el_list = selection.PickObjects(ObjectType.Element, filter_type, "Select Wall")
+            selected_els = [doc.GetElement(el) for el in el_list]
+
+        if not selected_els:
+            forms.alert('No element selected', exitscript=True)
+
+    return selected_els
